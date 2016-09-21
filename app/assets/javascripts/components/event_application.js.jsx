d8f06349bc30be05142ca0198e57bae15658ed4f
Chris Larsen <clarsenipod@gmail.com>
2016-09-21 18:52:39 -0400

    Complete Section 18

--- app/assets/javascripts/components/event_application.js.jsx
+++ app/assets/javascripts/components/event_application.js.jsx
@@ -16,13 +16,21 @@ var EventApplication = React.createClass({
         alert('Cannot get data from API: ', error);
       }
     });
-},
+  },
+  handleSearch: function(events) {
+      this.setState({ events: events });
+  },
   render: function() {
     return(
       <div className="container">
         <div className="jumbotron">
           <h1>ReactJS Tutorial</h1>
           <p>by Piotr Jaworski</p>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <SearchForm handleSearch={this.handleSearch} />
+          </div>
         </div>
         <div className="row">
           <div className="col-md-12">
